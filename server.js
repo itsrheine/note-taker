2a92a2309bd1f83eed225550aedeb339fd753c79
Marrione Nguyen <mtiamsic@gmail.com>
2020-10-02 15:48:11 -0700

    handle request for specific notes

--- server.js
+++ server.js
@@ -16,12 +16,27 @@ function filterByQuery(query, notesArray) {
     return filteredResults;
 }
 
+function findById(id, notesArray) {
+    const result = notesArray.filter(notes => notes.id === id)[0];
+    return result;
+}
+
+// Routes
 app.get('/api/notes', (req, res) => {
     let results = notes; 
     if (req.query) {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
 });
 
 app.listen(PORT, () => {
